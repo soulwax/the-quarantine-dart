3baceed68a149a4ee72be4614aab89d0d761aa4c
soulwax <soulwax-95@protonmail.com>
2026-09-12 04:40:12 +0200

    Update boot.js to load new module and WASM files

--- dist/web/boot.js
+++ dist/web/boot.js
@@ -2,8 +2,8 @@
   const app = './app/';
   if ((WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,0,0,1,5,1,95,1,120,0]))&&WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,0,0,1,5,1,96,0,1,123,3,2,1,0,10,10,1,8,0,65,0,253,15,253,98,11])))) {
     try {
-      const m = await import(app + 'main.a3fbb2a0.mjs');
-      const compiled = await m.compileStreaming(fetch(app + 'main.1e1ceab7.wasm'));
+      const m = await import(app + 'main.6607dcb2.mjs');
+      const compiled = await m.compileStreaming(fetch(app + 'main.51040e23.wasm'));
       (await compiled.instantiate({})).invokeMain();
       return;
     } catch (e) {
@@ -11,6 +11,6 @@
     }
   }
   const s = document.createElement('script');
-  s.src = app + 'main.f736c58f.js';
+  s.src = app + 'main.f32b24e9.js';
   document.body.appendChild(s);
 })();
